49043721ed5e4de017fe923c314658a0f74c37f1
yug pavasiya <yugpavasiya@outlook.com>
2024-05-28 13:04:19 +0530

    initial commit

---
+++ _code.py
@@ -0,0 +1,80 @@
+import cv2
+import mediapipe as mp
+import handtrackingmodule as htm
+import numpy as np
+import time
+import math
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+
+
+wCam,hCam=640,480
+
+cap = cv2.VideoCapture(0)
+cap.set(3,wCam)
+cap.set(4,hCam)
+
+pTime=0
+detector = htm.handDetector(detectionCon=0.8, maxHands=1)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+# volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange()
+# volume.SetMasterVolumeLevel(0, None)
+minVol = volRange[0]
+maxVol = volRange[1]
+
+
+while True:
+    success,img =cap.read()
+    image= detector.findHands(img)    
+    lmList= detector.findPosition(img,draw=False)
+    if len(lmList)!=0:
+        # print(lmList[4], lmList[8])
+
+        x1,y1 = lmList[4][1], lmList[4][2]
+        x2,y2 = lmList[8][1], lmList[8][2]
+        cx,cy =(x1+x2)//2, (y1+y2)//2
+
+
+        cv2.circle(img,(x1,y1),7,(255,0,255),cv2.FILLED)
+        cv2.circle(img,(x2,y2),7,(255,0,255),cv2.FILLED)
+        cv2.line(img,(x1,y1),(x2,y2),(255,0,255),2)
+        cv2.circle(img,(cx,cy),7,(255,0,0),cv2.FILLED)
+
+        length = math.hypot(x2-x1,y2-y1)
+
+        # range can differ for you
+        maxL = 125
+        minL=15
+        vol = np.interp(length,[minL,maxL],[minVol,maxVol])
+
+        ex =[int(length),vol]
+        print(ex)
+
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length<=minL:
+            cv2.circle(img,(cx,cy),7,(0,255,0),cv2.FILLED)
+
+        if length>=maxL:
+            cv2.circle(img,(cx,cy),7,(0,255,0),cv2.FILLED)
+
+        
+
+
+
+
+
+    cTime= time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+
+    cv2.putText(img,f'{int(fps)}',(40,50),cv2.FONT_HERSHEY_COMPLEX_SMALL,2,(255,0,255),2)
+
+
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
